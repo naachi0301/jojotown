af999183ecd58bab0a2e0aa200fc3f95d8f103ac
EC2 Default User <ec2-user@ip-172-31-8-194.ap-northeast-1.compute.internal>
2020-07-17 06:12:45 +0000

    商品追加フォーム制作

---
+++ resources/views/admin/product/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>商品追加フォーム</title>
+    </head>
+    <body>
+        <h1>商品追加フォーム</h1>
+        <form action="#" method="post">
+            <h4>商品名</h4>
+            <input type="text" name="product_name">
+        </form>
+        <form action="#" method="post">
+           <h4>商品説明</h4>
+           <textarea name="product_explain"></textarea>
+        </form>
+        <form action="#" method="post">
+           <h4>値段</h4>
+           <input type="text" name="price">
+        </form>
+        <input type="submit" value="送信">
+        <p></p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::get('/', 'WelcomeController@index');
+    
+
+Route::get('product/create', 'AdminProductController@index');
