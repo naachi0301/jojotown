36b40936fa55ecaada6d146597c06aa94687956b
EC2 Default User <ec2-user@ip-172-31-8-194.ap-northeast-1.compute.internal>
2020-07-17 06:46:57 +0000

    商品追加フォーム制作２

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -5,19 +5,21 @@
     </head>
     <body>
         <h1>商品追加フォーム</h1>
-        <form action="#" method="post">
-            <h4>商品名</h4>
-            <input type="text" name="product_name">
+        <form action="/form.php" method="post">
+            <div>
+                <label for="product_name">商品名</label>
+                <input type="text" id="product_name" name="product_name">
+            </div>
+            <div>
+                <label for="product_explain">商品説明<label>
+                <textarea id="product_explain" name="product_explain"></textarea>
+            </div>
+            <div>
+                <label for="price">値段</label>
+                <input type="text"　id="price" name="price">
+            </div>
+            <input type="submit" value="送信">
         </form>
-        <form action="#" method="post">
-           <h4>商品説明</h4>
-           <textarea name="product_explain"></textarea>
-        </form>
-        <form action="#" method="post">
-           <h4>値段</h4>
-           <input type="text" name="price">
-        </form>
-        <input type="submit" value="送信">
         <p></p>
     </body>
 </html>
