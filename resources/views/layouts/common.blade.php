5e543c8737e747f75bf367955fa7494c9a3705dd
EC2 Default User <ec2-user@ip-172-31-8-194.ap-northeast-1.compute.internal>
2020-08-01 04:52:57 +0000

    サーバー構築

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -4,18 +4,19 @@
         <meta charset="utf-8">
         <title>リアルタイムトレンド</title> 
         <link href="css/index.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
     <body>
 <!-----------------------------header----------------------------->
-        <div class="header">
-            <h1>リアルタイムトレンド</h1>
-        </div>
-<!-------------------------------main----------------------------->
-      @yield('content')
-<!---------------------------------------------footer---------------------------------------->
-        <div class="footer">
-            <p>©️2020 c-kusuda</p>
-        </div>
+        <header class="header fixed-top">
+            <p class="font-weight-bold text-nowrap text-dark">リアルタイムトレンド</p> 
+        </header>
+    <!-------------------------------main----------------------------->
+          @yield('content')
+    <!---------------------------------------------footer---------------------------------------->
+        <footer class="footer mb-0">
+            <p class="text-center font-weight-light">©️2020 c-kusuda</p>
+        </footer>
         <div><a href="/admin/product">管理機能</a></div>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 <!-------------------------------main----------------------------->
         <div class="table">
-            <ul>
+            <ul class="m-0 p-0 ">
                 <li><a href="#man">man</a></li>
                 <li><a href="#woman">woman</a></li>
                 <li><a href="#item">item</a></li>
@@ -18,6 +18,25 @@
             <div class="main">
                 
                 @foreach ($products as $product)
+                <div class="man_list" card style="width: 17rem;">
+                    <a href="{{ $product['url'] }}">
+                        <img class="card-img-top" src="{{ $product['image_url'] }}" alt="商品画像1">
+                    </a>
+                    <div class="card-body">
+                        <p class="card-title">{{ $product['name'] }}</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        </div>
+        <div id="woman">
+            <div class="headline">
+                <h2>man</h2>
+            </div>
+            <div class="main">
+                
+                @foreach ($products as $product)
                 <div class="man_list">
                     <a href="{{ $product['url'] }}">
                         <img src="{{ $product['image_url'] }}" alt="画像例1">
@@ -25,8 +44,40 @@
                     <p>{{ $product['name'] }}</p>
                 </div>
                 @endforeach
-                
             </div>
         </div>
-
+        </div>
+        <div id="item">
+            <div class="headline">
+                <h2>man</h2>
+            </div>
+            <div class="main">
+                
+                @foreach ($products as $product)
+                <div class="man_list">
+                    <a href="{{ $product['url'] }}">
+                        <img src="{{ $product['image_url'] }}" alt="画像例1">
+                    </a>
+                    <p>{{ $product['name'] }}</p>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        </div>
+        <div id="color">
+            <div class="headline">
+                <h2>man</h2>
+            </div>
+            <div class="main">
+                
+                @foreach ($products as $product)
+                <div class="man_list">
+                    <a href="{{ $product['url'] }}">
+                        <img src="{{ $product['image_url'] }}" alt="画像例1">
+                    </a>
+                    <p>{{ $product['name'] }}</p>
+                </div>
+                @endforeach
+            </div>
+        </div>
 @endsection
