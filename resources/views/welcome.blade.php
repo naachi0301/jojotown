128e2d270520b85790cee19e47c3232363e9740f
EC2 Default User <ec2-user@ip-172-31-8-194.ap-northeast-1.compute.internal>
2020-08-08 04:04:12 +0000

    cardに確認

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,15 +15,15 @@
             <div class="headline">
                 <h2>man</h2>
             </div>
-            <div class="main">
+            <div class=>
                 
                 @foreach ($products as $product)
-                <div class="man_list" card style="width: 17rem;">
+                <div class="man_list">
                     <a href="{{ $product['url'] }}">
-                        <img class="card-img-top" src="{{ $product['image_url'] }}" alt="商品画像1">
+                        <img src="{{ $product['image_url'] }}" alt="商品画像1">
                     </a>
-                    <div class="card-body">
-                        <p class="card-title">{{ $product['name'] }}</p>
+                    <div>
+                        <p>{{ $product['name'] }}</p>
                     </div>
                 </div>
                 @endforeach
@@ -34,7 +34,7 @@
             <div class="headline">
                 <h2>man</h2>
             </div>
-            <div class="main">
+            <div class="container">
                 
                 @foreach ($products as $product)
                 <div class="man_list">
@@ -51,11 +51,11 @@
             <div class="headline">
                 <h2>man</h2>
             </div>
-            <div class="main">
+            <div class="container">
                 
                 @foreach ($products as $product)
-                <div class="man_list">
-                    <a href="{{ $product['url'] }}">
+                <div class="man_list row">
+                    <a class="col-sm-3" href="{{ $product['url'] }}">
                         <img src="{{ $product['image_url'] }}" alt="画像例1">
                     </a>
                     <p>{{ $product['name'] }}</p>
@@ -68,7 +68,7 @@
             <div class="headline">
                 <h2>man</h2>
             </div>
-            <div class="main">
+            <div class="container">
                 
                 @foreach ($products as $product)
                 <div class="man_list">
